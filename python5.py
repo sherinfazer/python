36b59b46e31f6365a61749a080ffe63fe7d0a34e
sherinfazer <43169290+sherinfazer@users.noreply.github.com>
2018-09-17 11:51:58 +0530

    Add files via upload

---
+++ python5.py
@@ -0,0 +1,12 @@
+k1 = 3
+k2 = 67
+k3 = 51
+
+if (k1 > k2) and (k1 > k3):
+ biggest = num1
+elif (k2 > k1) and (k2 > k3):
+ biggest = k2
+else:
+ biggest = k3
+
+print("The biggest number between",k1,",",kr2,"and",k3,"is",biggest)
