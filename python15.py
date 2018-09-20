89fb46b870426159e8f7357cd2457ccf89b5c0d1
sherinfazer <43169290+sherinfazer@users.noreply.github.com>
2018-09-20 16:54:28 +0530

    Add files via upload

--- python15.py
+++ python15.py
@@ -1,5 +1,5 @@
-lower_limit = int(input("Enter the lower limit : "))
-upper_limit = int(input("Enter the upper limit : "))
-for i in range(lower_limit,upper_limit+1):
+low_limit = int(input("Enter the low limit : "))
+upp_limit = int(input("Enter the upp limit : "))
+for i in range(low_limit,upp_limit+1):
   if(i%2 == 0):
     print(i)
