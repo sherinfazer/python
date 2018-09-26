5593506a2d884360c344534a7acb4b29d8e95d12
sherinfazer <43169290+sherinfazer@users.noreply.github.com>
2018-09-26 15:14:06 +0530

    Add files via upload

---
+++ python21.py
@@ -0,0 +1,14 @@
+def sumOfAP( s, p,d) : 
+    sum = 0
+    i = 0
+    while i < n : 
+        sum = sum + a 
+        s = s + d
+        i = i + 1
+    return sum
+      
+# Driver function 
+s = 20
+d = 2.5
+p = 1.5
+print (sumOfAP(s, p, d)) 
