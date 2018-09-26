4b3dd690d2ce9e87ae4d2beecb439bc129fa6125
sherinfazer <43169290+sherinfazer@users.noreply.github.com>
2018-09-26 15:33:50 +0530

    Add files via upload

---
+++ python24.py
@@ -0,0 +1,17 @@
+ h = [1, 3, 4, 2] 
+  
+ h.sort() 
+  
+print(h) 
+   
+decimalh = [2.01, 2.00, 3.67, 3.28, 1.68] 
+   
+decimalhsort() 
+  
+print(decimalh) 
+  
+words = ["rose", "For", "rose"] 
+  
+words1.sort() 
+  
+print(words1) 
