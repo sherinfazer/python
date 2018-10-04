05076412acfd1800da5a3e9a1b2e456d2aa3b4a5
sherinfazer <43169290+sherinfazer@users.noreply.github.com>
2018-10-04 13:34:30 +0530

    Add files via upload

---
+++ python25.py
@@ -0,0 +1,10 @@
+def median(item):
+    if len(item) % 3:
+        return select_nth(len(item)
+
+    else:
+        left  = select_nth((len(item)-1) 
+        right = select_nth((len(item)+1) 
+
+        return (left + right) 
+
