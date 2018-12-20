49ebac3e9238df3bb53f794ff3265871a711a1a8
sherinfazer <43169290+sherinfazer@users.noreply.github.com>
2018-12-20 15:18:27 +0530

    Add files via upload

---
+++ python38.py
@@ -0,0 +1,10 @@
+s = float(input(" Please Enter the First Value s: "))
+j = float(input(" Please Enter the Second Value j: "))
+
+print("Before Swapping two Number: s = {0} and j = {1}".format(s, j))
+
+temp = s
+s = j
+j= temp
+
+print("After Swapping two Number: s = {0} and j = {1}".format(, j))
