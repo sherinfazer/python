d60411a836ce465dd631dcfb83cf818469d551cb
sherinfazer <43169290+sherinfazer@users.noreply.github.com>
2018-12-20 14:22:12 +0530

    Add files via upload

---
+++ python30.py
@@ -0,0 +1,19 @@
+def td_format(td_object):
+    sec = int(td_object.total_sec())
+    periods = [
+        ('year',        60*60*24*365),
+        ('month',       60*60*24*30),
+        ('day',         60*60*24),
+        ('hour',        60*60),
+        ('minute',      60),
+        ('second',      1)
+    ]
+
+    strings=[]
+    for period_name, period_sec in periods:
+        if sec > period_sec:
+            period_value , sec = divmod(sec, period_sec)
+            has_s = 's' if period_value > 1 else ''
+            strings.append("%s %s%s" % (period_value, period_name, has_s))
+
+    return ", ".join(strings)
