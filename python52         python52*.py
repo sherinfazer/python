5268ed9fb3d92dfa7d860aad52472cf122207591
sherinfazer <43169290+sherinfazer@users.noreply.github.com>
2018-12-31 15:04:26 +0530

    Rename python52.py to python52         python52*.py

---
+++ python52         python52*.py
@@ -0,0 +1,21 @@
+n=input()
+if(n==1):
+    print("ONE")
+elif(n==2):
+    print("TWO")
+elif(n==3):
+    print("THREE")
+elif(n==4):
+    print("FOUR")
+elif(n==5):
+    print("FIVE")
+elif(n==6):
+    print("SIX")
+elif(n==7):
+    print("SEVEN")
+elif(n==8):
+    print("EIGHT")
+elif(n==9):
+    print("NINE")
+else:
+    print("TEN")
