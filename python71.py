fedd6e904bf5e430df3ffa19c88e1d128d52a49c
sherinfazer <43169290+sherinfazer@users.noreply.github.com>
2019-01-07 14:11:27 +0530

    Create python71.py

---
+++ python71.py
@@ -0,0 +1,13 @@
+
+a=input()
+def pal(x):
+	a=x[::-1]
+	return c
+x=pal(a)
+if b.isalpha():
+	if(x==a):
+		print("YES a Palindrome")
+	else:
+		print("Not a palindrome")
+else:
+	print("INVALID INPUT")
