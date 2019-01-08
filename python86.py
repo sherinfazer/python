4ecb396ce2e3af24d673b0653a2b6ad91a520032
sherinfazer <43169290+sherinfazer@users.noreply.github.com>
2019-01-08 11:44:12 +0530

    Create python86.py

---
+++ python86.py
@@ -0,0 +1,16 @@
+
+def1 isogram():
+	m=input()
+	n1=list(s)
+	o1=[]
+	for i in l1:
+		if not i in r1:
+			r1.append(i)
+	if len(l1)==len(r1):
+		print('yes')
+	else :
+		print('no')
+try:
+	isogram()
+except:
+	print('invalid')
